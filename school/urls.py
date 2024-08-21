9880b57d33d14cdf3ad19b70c3c89a46c0c42b85
Manoj-Ghosh <manojghoshdk@gmail.com>
2024-08-21 23:58:00 +0530

    content added in views.py and urls.py file added in school app and content also added

---
+++ school/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('dashboard/', views.dashboard, name='dashboard'),
+    path('students/', views.students, name='students'),
+    path('classes/', views.classes, name='classes'),
+    path('grades/', views.grades, name='grades'),
+    path('attendance/', views.attendance, name='attendance'),
+]
--- school/views.py
+++ school/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.shortcuts import render, redirect
+from .models import Student, Class, Grade, Attendance
+
+def index(request):
+    return render(request, 'index.html')
+
+def dashboard(request):
+    return render(request, 'dashboard.html')
+
+def students(request):
+    students = Student.objects.all()
+    return render(request, 'students.html', {'students': students})
+
+def classes(request):
+    classes = Class.objects.all()
+    return render(request, 'classes.html', {'classes': classes})
+
+def grades(request):
+    grades = Grade.objects.all()
+    return render(request, 'grades.html', {'grades': grades})
+
+def attendance(request):
+    attendance_records = Attendance.objects.all()
+    return render(request, 'attendance.html', {'attendance': attendance_records})
