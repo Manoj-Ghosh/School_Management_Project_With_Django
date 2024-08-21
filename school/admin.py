1ea120308a5f500d6d3eb4c62521e64c89edab20
Manoj-Ghosh <manojghoshdk@gmail.com>
2024-08-21 22:04:27 +0530

    content added inside admin.py under school app

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from django.contrib import admin
+from .models import Student, Class, Grade, Attendance
+
+admin.site.register(Student)
+admin.site.register(Class)
+admin.site.register(Grade)
+admin.site.register(Attendance)
