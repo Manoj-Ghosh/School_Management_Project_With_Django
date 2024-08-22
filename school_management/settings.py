edf6c9bdcb5f95e018c6d92f803e64c498f49340
Manoj-Ghosh <manojghoshdk@gmail.com>
2024-08-22 18:24:27 +0530

    Make and apply migrations to create the database tables.

--- school_management/settings.py
+++ school_management/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'school'
+    'school',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
