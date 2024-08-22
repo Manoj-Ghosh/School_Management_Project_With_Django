1618570c4d8ee5783bbed760a334dab62ef6d4a9
Manoj-Ghosh <manojghoshdk@gmail.com>
2024-08-22 20:48:19 +0530

    content of views.py changed

--- school/views.py
+++ school/views.py
@@ -1,28 +1,42 @@
-from django.shortcuts import render
-
-# Create your views here.
-
 from django.shortcuts import render, redirect
-from .models import Student, Class, Grade, Attendance
+from .forms import StudentForm, ClassForm, GradeForm, AttendanceForm
 
-def index(request):
-    return render(request, 'index.html')
+def add_student(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('students')
+    else:
+        form = StudentForm()
+    return render(request, 'add_student.html', {'form': form})
 
-def dashboard(request):
-    return render(request, 'dashboard.html')
+def add_class(request):
+    if request.method == 'POST':
+        form = ClassForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('classes')
+    else:
+        form = ClassForm()
+    return render(request, 'add_class.html', {'form': form})
 
-def students(request):
-    students = Student.objects.all()
-    return render(request, 'students.html', {'students': students})
+def add_grade(request):
+    if request.method == 'POST':
+        form = GradeForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('grades')
+    else:
+        form = GradeForm()
+    return render(request, 'add_grade.html', {'form': form})
 
-def classes(request):
-    classes = Class.objects.all()
-    return render(request, 'classes.html', {'classes': classes})
-
-def grades(request):
-    grades = Grade.objects.all()
-    return render(request, 'grades.html', {'grades': grades})
-
-def attendance(request):
-    attendance_records = Attendance.objects.all()
-    return render(request, 'attendance.html', {'attendance': attendance_records})
+def add_attendance(request):
+    if request.method == 'POST':
+        form = AttendanceForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('attendance')
+    else:
+        form = AttendanceForm()
+    return render(request, 'add_attendance.html', {'form': form})
